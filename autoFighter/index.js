277343f162f17cfe694f32e4afd600d1db9c1e5f
Gabriel <Gabrielyidali@gmail.com>
2020-10-07 16:19:07 -0400

    finished autoAttacker and nightmareBot

--- autoFighter/index.js
+++ autoFighter/index.js
@@ -6,74 +6,145 @@ function main() {
   sleep(2000);
 
   while (true) {
-    attack();
-    sleep(20000);
-  }
-}
-function attack() {
-  var finder = findTarget();
-  robot.moveMouse(finder.random_x, finder.random_y);
-  robot.mouseClick('right');
-  sleep(601);
-  git;
-  robot.moveMouseSmooth(finder.random_x, finder.random_y + 27);
-  robot.mouseClick('left');
-}
-function timer() {
-  var img = robot.screen.capture(0, 0, 300, 300);
-  var health = img.colorAt(28, 76);
-  var man = ['621313'];
-  if (man.includes(health)) {
-    return true;
-  }
-}
-function redTimer() {
-  var img = robot.screen.capture(0, 0, 300, 300);
-  var health = img.colorAt(28, 76);
-  var man = ['621313'];
-  if (man.includes(health)) {
-    return false;
-  }
-}
-function findTarget() {
-  var width = 520;
-  var height = 365;
-  var img = robot.screen.capture(0, 0, width, height);
-  var man = ['00ffff'];
-  for (var i = 0; i < 1011; i++) {
-    var random_x = getRandomInt(0, width - 1);
-    var random_y = getRandomInt(0, height - 1);
-    var sample_color = img.colorAt(random_x, random_y);
-
-    if (man.includes(sample_color)) {
-      console.log('found at : ' + random_x + ' ' + random_y);
-      return { random_x, random_y };
+    while (true) {
+      var find = findTarget();
+      if (find == false) {
+        console.log('Could not find a target ');
+        break;
+      }
+      robot.moveMouse(find.random_x, find.random_y);
+      robot.mouseClick('left');
+      // robot.mouseClick('right');
+      // sleep(940);
+      // robot.moveMouseSmooth(find.random_x, find.random_y + 27);
+      // robot.mouseClick('left');
+      sleep(1270);
+      enemyHealth();
+      runBacktoCenter();
     }
+    sleep(4000);
   }
 }
 
 function sleep(ms) {
   Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
 }
-function waiting() {
-  var random_x = getRandomInt(0, 10);
-  sleep(random_x * 1000);
-}
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function findRoom() {
-  var img = robot.screen.capture(x, y, width, height);
-  var man = ['00ffff'];
-  for (var i = 0; i < 100; i++) {}
-}
-function screen() {
-  var img = robot.screen.capture(0, 0, 1920, 1080);
-  var color = img.colorAt(30, 18);
-  console.log(color);
-}
 
+function findTarget() {
+  var img = robot.screen.capture(0, 0, 520, 366);
+  var targetNpc = ['00ffff'];
+  for (var i = 0; i < 10011; i++) {
+    var random_x = getRandomInt(0, 500);
+    var random_y = getRandomInt(0, 360);
+    var sample_color = img.colorAt(random_x, random_y);
+
+    if (targetNpc.includes(sample_color)) {
+      console.log('found at : ' + random_x + ' ' + random_y);
+      console.log('target found');
+
+      return { random_x, random_y };
+    }
+  }
+  return false;
+}
+function ownhealthbar() {
+  // health bar dimension  20-135
+  // heigth 75 82
+  var img = robot.screen.capture(0, 0, 1980, 1080);
+  var redBar = [
+    '611211',
+    '611212',
+    '611213',
+    '611214',
+    '621410',
+    '621411',
+    '621412',
+    '621413',
+    '621311',
+    '621312',
+    '621313',
+    '621314',
+    '631410',
+    '631411',
+    '631412',
+    '631414',
+    '631512',
+    '641513',
+    '641514',
+    '641515',
+    '641516',
+    '651615',
+  ];
+
+  var sample_color1 = img.colorAt(32, 78);
+  console.log(sample_color1);
+  var sample_color2 = img.colorAt(53, 78);
+  console.log(sample_color2);
+  var sample_color3 = img.colorAt(74, 72);
+  console.log(sample_color3);
+  var sample_color4 = img.colorAt(98, 78);
+  console.log(sample_color4);
+  var sample_color5 = img.colorAt(122, 78);
+  console.log(sample_color5);
+  console.log(sample_color1);
+
+  if (redBar.includes(sample_color1)) {
+    console.log('health is below 20%');
+  } else if (redBar.includes(sample_color2)) {
+    console.log('health is below 40%');
+  } else if (redBar.includes(sample_color3)) {
+    console.log('health is below 60%');
+  } else if (redBar.includes(sample_color4)) {
+    console.log('health is below 80%');
+  } else if (redBar.includes(sample_color5)) {
+    console.log('health is full');
+  }
+}
+function runBacktoCenter() {
+  var img = robot.screen.capture(0, 0, 520, 366);
+  var targetNpc = ['cfcf00'];
+  for (var i = 0; i < 10011; i++) {
+    var random_x = getRandomInt(0, 450);
+    var random_y = getRandomInt(0, 300);
+    var sample_color = img.colorAt(random_x, random_y);
+
+    if (targetNpc.includes(sample_color)) {
+      console.log('repositioning');
+
+      robot.moveMouseSmooth(random_x, random_y);
+      robot.mouseClick('left');
+      sleep(712);
+      break;
+    }
+  }
+}
+function enemyHealth() {
+  while (true) {
+    sleep(1830);
+    var img = robot.screen.capture(0, 0, 150, 100);
+    var greenBar = ['0a8633'];
+    var sample_color = img.colorAt(20, 78);
+    var sample_color1 = img.colorAt(50, 78);
+    var sample_color2 = img.colorAt(100, 78);
+
+    if (greenBar.includes(sample_color2)) {
+      console.log('attacking opponent');
+    } else if (greenBar.includes(sample_color1)) {
+      console.log('enemy is above 40% health');
+    } else if (greenBar.includes(sample_color)) {
+      console.log('enemy is below 20% health');
+    } else {
+      break;
+    }
+    sleep(6000);
+  }
+}
+//healthbar();
 main();
-//screen();
+//
--- nightmareZone/index.js
+++ nightmareZone/index.js
@@ -6,74 +6,77 @@ function main() {
   sleep(2000);
 
   while (true) {
-    attack();
-    sleep(20000);
-  }
-}
-function attack() {
-  var finder = findTarget();
-  robot.moveMouse(finder.random_x, finder.random_y);
-  robot.mouseClick('right');
-  sleep(601);
-  git;
-  robot.moveMouseSmooth(finder.random_x, finder.random_y + 27);
-  robot.mouseClick('left');
-}
-function timer() {
-  var img = robot.screen.capture(0, 0, 300, 300);
-  var health = img.colorAt(28, 76);
-  var man = ['621313'];
-  if (man.includes(health)) {
-    return true;
-  }
-}
-function redTimer() {
-  var img = robot.screen.capture(0, 0, 300, 300);
-  var health = img.colorAt(28, 76);
-  var man = ['621313'];
-  if (man.includes(health)) {
-    return false;
-  }
-}
-function findTarget() {
-  var width = 520;
-  var height = 365;
-  var img = robot.screen.capture(0, 0, width, height);
-  var man = ['00ffff'];
-  for (var i = 0; i < 1011; i++) {
-    var random_x = getRandomInt(0, width - 1);
-    var random_y = getRandomInt(0, height - 1);
-    var sample_color = img.colorAt(random_x, random_y);
+    var j = 0;
+    while (j < 6) {
+      var random_x = getRandomInt(30000, 38520);
 
-    if (man.includes(sample_color)) {
-      console.log('found at : ' + random_x + ' ' + random_y);
-      return { random_x, random_y };
+      robot.moveMouse(580, 294);
+      robot.mouseClick('right');
+      sleep(642);
+      robot.moveMouseSmooth(580, 338);
+      robot.mouseClick();
+
+      sleep(4129);
+      prayerToggle();
+
+      j++;
+      console.log('sleeping for ' + random_x + ' ms');
+      sleep(random_x);
     }
+    battleaxe();
   }
 }
 
 function sleep(ms) {
   Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
+  // console.log('sleeping for ' + ms + ' ms');
 }
-function waiting() {
-  var random_x = getRandomInt(0, 10);
-  sleep(random_x * 1000);
+function battleaxe() {
+  robot.moveMouseSmooth(623, 289);
+  robot.mouseClick();
+  sleep(923);
+  robot.moveMouseSmooth(589, 175);
+  robot.mouseClick();
+  sleep(523);
+  robot.moveMouseSmooth(623, 289);
+  robot.mouseClick();
+  console.log('using my battleaxe spec ');
+}
+
+function prayerToggle() {
+  robot.moveMouseSmooth(557, 121);
+  robot.mouseClick();
+  sleep(131);
+  robot.mouseClick();
+}
+
+function tick() {
+  console.log('1tick');
+  sleep(601);
+}
+function l1() {
+  var random_x = getRandomInt(420, 699);
+}
+
+function la2() {
+  var random_x = getRandomInt(4271, 10000);
+}
+function lay3() {
+  var random_x = getRandomInt(0, 414);
+}
+function laye4() {
+  var random_x = getRandomInt(14103, 31413);
+}
+function layer5() {
+  // sleeping for between 18.794 millisecond to 42.526 milliseconds
+  l1();
+  la2();
+  lay3();
+  laye4();
 }
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function findRoom() {
-  var img = robot.screen.capture(x, y, width, height);
-  var man = ['00ffff'];
-  for (var i = 0; i < 100; i++) {}
-}
-function screen() {
-  var img = robot.screen.capture(0, 0, 1920, 1080);
-  var color = img.colorAt(30, 18);
-  console.log(color);
-}
-
 main();
-//screen();
