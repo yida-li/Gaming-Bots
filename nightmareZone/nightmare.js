7991055462e421cdc74b2e25dc8d44f3e3ab832d
yida-li <gabrielyidali@gmail.com>
2020-10-08 12:42:20 -0400

    agility

--- nightmareZone/nightmare.js
+++ nightmareZone/nightmare.js
@@ -1,4 +1,5 @@
 //importing the robotjs
+
 var robot = require('robotjs');
 
 function main() {
@@ -10,10 +11,10 @@ function main() {
     while (j < 6) {
       var random_x = getRandomInt(30000, 38520);
 
-      robot.moveMouse(580, 294);
+      robot.moveMouse(583, 294);
       robot.mouseClick('right');
       sleep(642);
-      robot.moveMouseSmooth(580, 338);
+      robot.moveMouseSmooth(583, 338);
       robot.mouseClick();
 
       sleep(4129);
@@ -27,48 +28,57 @@ function main() {
   }
 }
 
+// function that performs wait for a thread/process/program
 function sleep(ms) {
   Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
   // console.log('sleeping for ' + ms + ' ms');
 }
+// function that performs strength boost
 function battleaxe() {
-  robot.moveMouseSmooth(623, 289);
+  robot.moveMouseSmooth(623, 254);
   robot.mouseClick();
   sleep(923);
   robot.moveMouseSmooth(589, 175);
   robot.mouseClick();
   sleep(523);
-  robot.moveMouseSmooth(623, 289);
+  robot.moveMouseSmooth(623, 254);
   robot.mouseClick();
   console.log('using my battleaxe spec ');
 }
-
+// function that resets health restore
 function prayerToggle() {
   robot.moveMouseSmooth(557, 121);
   robot.mouseClick();
   sleep(131);
   robot.mouseClick();
 }
-
+// function that displays game tick
 function tick() {
   console.log('1tick');
   sleep(601);
 }
+
+// a subset of layers to differentiate time delay
 function l1() {
   var random_x = getRandomInt(420, 699);
+  sleep(random_x);
 }
-
 function la2() {
   var random_x = getRandomInt(4271, 10000);
+  sleep(random_x);
 }
 function lay3() {
   var random_x = getRandomInt(0, 414);
+  sleep(random_x);
 }
 function laye4() {
   var random_x = getRandomInt(14103, 31413);
+  sleep(random_x);
 }
+// function that calls all layer
+// sleeping for between 18.794 millisecond to 42.526 milliseconds
+// used if necessary
 function layer5() {
-  // sleeping for between 18.794 millisecond to 42.526 milliseconds
   l1();
   la2();
   lay3();
@@ -79,4 +89,5 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 main();
