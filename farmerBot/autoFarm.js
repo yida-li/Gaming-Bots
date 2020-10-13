7513331e33a79659daa98b8df61cb899951f2daf
yida-li <gabrielyidali@gmail.com>
2020-10-13 15:46:02 -0400

    added auto combination feature

--- farmerBot/autoFarm.js
+++ farmerBot/autoFarm.js
@@ -1,18 +1,84 @@
-
+// the only dependencies needed for this segment of my consciousnesss
 var robot = require('robotjs');
-
-
 
 // function that performs wait for a thread/process/program
 function sleep(ms) {
-    console.log('sleeping for ' + ms + ' ms');
+  console.log('sleeping for ' + ms + ' ms');
   Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
   // console.log('sleeping for ' + ms + ' ms');
 }
-/*
-(function eden(){
+
+
+
+// combining infinite herbs
+function 
+GatherResources(){
+
+while(true){
+var X_herbArray=[getRandomInt(576,586),getRandomInt(576,586),getRandomInt(576,586),getRandomInt(576,586),getRandomInt(576,586),getRandomInt(576,586),getRandomInt(576,586),
+    getRandomInt(616,630),  getRandomInt(616,630),   getRandomInt(616,630),  getRandomInt(616,630),   getRandomInt(616,630),  getRandomInt(616,630),  getRandomInt(616,630), 
+     getRandomInt(656,670) ,  getRandomInt(656,670) ,  getRandomInt(656,670) ,  getRandomInt(656,670) ,  getRandomInt(656,670) ,  getRandomInt(656,670) ,  getRandomInt(656,670) , 
+     getRandomInt(699,716),getRandomInt(699,716),getRandomInt(699,716),getRandomInt(699,716),getRandomInt(699,716),getRandomInt(699,716),getRandomInt(699,716)]
+var Y_herbArray=[ getRandomInt(247,258),getRandomInt(283,296),getRandomInt(321,332),getRandomInt(358,371),getRandomInt(389,406)
+,getRandomInt(431,444),getRandomInt(463,479),getRandomInt(247,258),getRandomInt(283,296),getRandomInt(321,332),getRandomInt(358,371),getRandomInt(389,406)
+,getRandomInt(431,444),getRandomInt(463,479),getRandomInt(247,258),getRandomInt(283,296),getRandomInt(321,332),getRandomInt(358,371),getRandomInt(389,406)
+,getRandomInt(431,444),getRandomInt(463,479),getRandomInt(247,258),getRandomInt(283,296),getRandomInt(321,332),getRandomInt(358,371),getRandomInt(389,406)
+,getRandomInt(431,444),getRandomInt(463,479)]
+  robot.moveMouseSmooth(getRandomInt(227,243),getRandomInt(120,129));
+robot.mouseClick('right');
+tick(getRandomInt(700,800));
+robot.moveMouseSmooth(getRandomInt(227,243),getRandomInt(213,214));
+robot.mouseClick();
+//close bank
+tick(getRandomInt(700,800));
+robot.moveMouseSmooth(getRandomInt(282,289),getRandomInt(120,129));
+robot.mouseClick();
+//close bank
+
+robot.moveMouseSmooth(getRandomInt(484,494),getRandomInt(43,53));
+robot.mouseClick();
+tick();
+
+//combine 2 elements
+robot.moveMouseSmooth(getRandomInt(576,586),getRandomInt(358,371));
+robot.mouseClick();
+robot.moveMouseSmooth(getRandomInt(576,586),getRandomInt(389,406));
+robot.mouseClick();
+tick(622);
+// clicking make all
+robot.moveMouseSmooth(getRandomInt(227,290),getRandomInt(433,480));
+robot.mouseClick();
+
+sleep(9000);
+robot.moveMouseSmooth(264,172);
+robot.mouseClick();
+
+
+sleep(getRandomInt(1200,2000));
+//bankcccc
+robot.moveMouseSmooth(getRandomInt(432,454),getRandomInt(327,342))
+robot.mouseClick();
+
+
+
+
+
+
+}
+
+ 
+}
+
+
+
+
+
+
+
+
+function CleaningVesispianGase(){
     var yida=0;
-for(var i=0;i<40;i++){
+for(var i=0;i<40000;i++){
 //take herb at 4th location
 robot.moveMouseSmooth(getRandomInt(227,243),getRandomInt(120,129));
 robot.mouseClick();
@@ -22,7 +88,7 @@ robot.moveMouseSmooth(getRandomInt(484,494),getRandomInt(43,53));
 robot.mouseClick();
 
 
-sleep(getRandomInt(600,1200));
+
 sleep(getRandomInt(600,1200));
 
 
@@ -55,7 +121,7 @@ var counter =0;
 
 
          }
-         sleep(getRandomInt(4000,5000));
+         sleep(getRandomInt(2000,2500));
 
          //banker
 robot.moveMouseSmooth(264,172);
@@ -66,9 +132,9 @@ sleep(getRandomInt(1200,2000));
 //bankcccc
 robot.moveMouseSmooth(getRandomInt(432,454),getRandomInt(327,342))
 robot.mouseClick();
-sleep(getRandomInt(600,1200));
-sleep(getRandomInt(600,1200));
-sleep(getRandomInt(600,1200));
+ 
+
+
 yida++;
 console.log(yida+' cycle sucessful');
 }
@@ -77,9 +143,9 @@ console.log(yida+' cycle sucessful');
 
 }
 
-)()  
 
-*/
+
+
 function swip(){
     console.log('swiping digital farms');
 var X_herbArray=[getRandomInt(576,586),getRandomInt(576,586),getRandomInt(576,586),getRandomInt(576,586),getRandomInt(576,586),getRandomInt(576,586),getRandomInt(576,586),
@@ -152,4 +218,6 @@ function getRandomInt(min, max) {
 }
 
 //main();
-swip();
+//swip();
+GatherResources();
+//CleaningVesispianGase();
