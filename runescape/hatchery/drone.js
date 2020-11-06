58fce71e62f703369498ed2d32e1ee907bf03097
Yida Li <gabrielyidali@example.com>
2020-11-06 09:20:30 -0500

    1step closer to rehobaom

--- runescape/hatchery/drone.js
+++ runescape/hatchery/drone.js
@@ -49,17 +49,40 @@ robot.mouseClick();
 
 
 function // clicks and initializes exchanging items
-exchange(){
+exchange(x){
 robot.moveMouseSmooth(getRandomInt(190,443),getRandomInt(200,213));
 robot.mouseClick();
 
-sleep(1000);
+sleep(3000);
 
 robot.moveMouseSmooth(getRandomInt(45,71),getRandomInt(163,175));
+robot.mouseClick();
+sleep(3000);
+
+robot.typeString(x);
 sleep(1000);
+robot.keyTap("enter");
 
-robot.typeString('grimy harralander');
+sleep(3000);
 
+// custom amount
+robot.moveMouseSmooth(getRandomInt(225,244),getRandomInt(234,247));
+robot.mouseClick();
+sleep(3000);
+
+// 100 amount
+robot.typeString('100');
+sleep(1000);
+robot.keyTap('enter');
+sleep(3000);
+
+//5 percent less
+robot.moveMouseSmooth(getRandomInt(284,302),getRandomInt(234,247))
+robot.mouseClick();
+sleep(1000);
+//confirm
+robot.moveMouseSmooth(getRandomInt(201,322),getRandomInt(304,324));
+robot.mouseClick();
 }
 
 
@@ -71,7 +94,7 @@ robot.typeString('grimy harralander');
 // the only thing stopping me was my own ego, super ego and the voices of others that are drowning my own inner desires 
 //
 //
-//
+//  login();
 
-login();
+exchange('grimy harralander');
 //
